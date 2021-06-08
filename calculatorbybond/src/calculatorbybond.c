fd34b703e137ced660d8b16844956e419ecbcdbc
artsobad <78956327+artsobad@users.noreply.github.com>
2021-06-08 15:14:33 +0300

    input output

--- calculatorbybond/src/calculatorbybond.c
+++ calculatorbybond/src/calculatorbybond.c
@@ -1,210 +1,168 @@
-/*
- ============================================================================
- Name        : calculatorbybond.c
- Author      : Nikita Bondarev
- ============================================================================
- */
-#include <stdio.h>  
-#include <stdlib.h> 
 
-int main() 
-{
-    setvbuf(stdout, NULL, _IONBF, 0); 
-    setvbuf(stderr, NULL, _IONBF, 0); 
+#include <stdio.h>
+#include <stdlib.h>
 
-    float a, b, *c, *d, res, g;
-  	char y, x, z, t, input[50], output[50], yy;
-  	int res1, i, j;
+int main() {
+    setvbuf(stdout, NULL, _IONBF, 0);
+    setvbuf(stderr, NULL, _IONBF, 0);
+    char in[100],out[100],vbor,operation,next;
+    FILE *inFile,*outFile;
+    float *onevector,*twovector;
+    int size;
+    float a, b, res, stepen;
+    printf("you want to work with vectors ");
+    printf("Hello , i am cancilator\n");
+    printf("My operation:\n");
+    printf("+- you can add two numbers ( 1 + 2 ) or minus ( 3 - 2 )\n");
+    printf("* or / you can multiply ( 1 * 3 ) or divide it ( 1 / 2 )\n ");
+    printf("^ you can raise to a power even to a negative one ( 2 ^ 3 ) , ( 5 ^ -2 )\n");
+    printf("! you can find the factorial of a number ( 5 ! )\n");
+    printf("Also, the calculator works with vectors,you can (+ - *)\n\n");
+    do{
+        printf("Enter input file name: ");         // просит ввести файл откуда брать данные
+        scanf("%s", in);
+        inFile = fopen(in,"r");
+        if (inFile == NULL){                     // проверка на существование файла ввода
+            printf("Error enter again\n");
+            while (inFile == NULL){
+                printf("Enter input file name: ");
+                scanf("%s", in);
+                inFile = fopen(in,"r");
+            }
+        }
+        printf("\nEnter output file name: ");   //просит ввести файл куда записовать данные
+        scanf("%s", out);
+        outFile = fopen(out,"w");
+        do {
+            fprintf(outFile,"What will you work with with numbers or vectors?  (s - numbers, v-vectors)\n"); // выбор с чем работать с числами или с векторами
+            fscanf(inFile," %c",&vbor);     // читает с файла с чем работать(с векторами или числами)
+            fprintf(outFile,"%c\n",vbor);
+            if(vbor == 'v'){
+                fscanf(inFile," %c",&operation); // читает с файла операцию
+                fprintf(outFile,"Enter the size of the vector:\n");
+                fscanf(inFile,"%i",&size);        // читает с файла размер вектора
+                fprintf(outFile,"%i\n",size);
+                onevector = malloc(size*sizeof(int));
+                twovector = malloc(size*sizeof(int));
+                fprintf(outFile,"Enter vector one:\n");
+                for(int i = 0; i < size ;i++){fscanf(inFile," %f",&onevector[i]);fprintf(outFile,"%f\n",onevector[i]);} // заполняем первый вектор
+                fprintf(outFile,"Enter vector two:\n");
+                for(int i = 0; i < size ;i++){fscanf(inFile," %f",&twovector[i]);fprintf(outFile,"%f\n",twovector[i]);} // заполняем второй вектор
+                fprintf(outFile,"Enter the operation on the vectors(+ - *):\n");
+                fprintf(outFile,"%c\n",operation);     // выводит в файл операцию
+                switch(operation){      // операции с векторами
+                    case('+'):
+                        fprintf(outFile, "( ");
+                        for (int i = 0; i < size; i++)
+                            fprintf(outFile, "%.2f ", onevector[i]);
+                        fprintf(outFile, ") + ( ");
+                        for (int i = 0; i < size; i++)
+                            fprintf(outFile, "%.2f ", twovector[i]);
+                        fprintf(outFile, ") = ( ");
+                        for (int i = 0; i < size; i++)
+                            fprintf(outFile, "%.2f ", onevector[i] + twovector[i]);
+                        fprintf(outFile, ")");
+                        fprintf(outFile,"\n\n");
+                        break;
+                    case('-'):
+                        fprintf(outFile, "( ");
+                        for (int i = 0; i < size; i++)
+                            fprintf(outFile, "%.2f ", onevector[i]);
+                        fprintf(outFile, ") + ( ");
+                        for (int i = 0; i < size; i++)
+                            fprintf(outFile, "%.2f ", twovector[i]);
+                        fprintf(outFile, ") = ( ");
+                        for (int i = 0; i < size; i++)
+                            fprintf(outFile, "%.2f ", onevector[i] - twovector[i]);
+                        fprintf(outFile, ")");
+                        fprintf(outFile,"\n\n");
+                        break;
+                    case('*'):
+                        fprintf(outFile, "( ");
+                        for (int i = 0; i < size; i++)
+                            fprintf(outFile, "%.2f ", onevector[i]);
+                        fprintf(outFile, ") * ( ");
+                        for (int i = 0; i < size; i++)
+                            fprintf(outFile, "%.2f ", twovector[i]);
+                        fprintf(outFile, ") = ");
+                        for (int i = 0; i < size; ++i) {
+                            if (i != size - 1 ){fprintf(outFile, "%.2f ", onevector[i]*twovector[i]);
+                                fprintf(outFile,"+ ");
+                            }
+                            else{
+                                fprintf(outFile, "%.2f ", onevector[i]*twovector[i]);
+                            }
+                        }
+                        fprintf(outFile, " = ");
+                        for (int i = 0; i < size; i++)
+                            res += onevector[i] * twovector[i];
+                        fprintf(outFile, "%.2f ", res);
+                        fprintf(outFile,"\n\n");
+                        break;
+                    default:                                 // если операция не определена то будет ошибка
+                        printf("Error\n");
+                }
+                free(onevector);            //очистка памяти
+                free(twovector);
+            }else{
+                fscanf(inFile," %c",&operation);        // читает с файла операцию
+                if (operation != '!') {                      // тут необходимое условие тк для факториала нужно только ввести число и операцию
+                    fscanf(inFile," %f %f",&a,&b);
+                    switch (operation) {                     //тут происходит выбор операции над числом или числами
+                        case ('+'):
+                            fprintf(outFile,"%f %c %f = %f\n", a, operation, b, a + b);
+                            fprintf(outFile,"\n");
+                            break;
+                        case ('-'):
+                            fprintf(outFile,"%f %c %f = %f\n", a, operation, b, a - b);
+                            fprintf(outFile,"\n");
+                            break;
+                        case ('/'):
+                            fprintf(outFile,"%f %c %f = %f\n", a, operation, b, a / b);
+                            fprintf(outFile,"\n");
+                            break;
+                        case ('*'):
+                            fprintf(outFile,"%f %c %f = %f\n", a, operation, b, a * b);
+                            fprintf(outFile,"\n");
+                            break;
+                        case ('^'):                              // помимо простых действий над число ( + - * /),
+                            stepen = 1;                          //где нужно ничего особенно пиcать для реализации
+                            if (b >= 0) {                        // в отличие от возведения в степень,где для реализации нужен цикл for
+                                for (int i = 1; i <= b; i++) {
+                                    stepen *= a;
+                                }
+                                fprintf(outFile,"%f %c %f = %f\n", a, operation, b, stepen);
+                            } else if (b < 0) {
+                                for (int i = 0; i > b; i--) {
+                                    stepen *= a;
+                                }
+                                fprintf(outFile,"%f %c %f = %f\n", a, operation, b, 1 / stepen);
 
-                FILE *in, *out; 
-		in = fopen(input, "r"); 
-		out = fopen(output, "w"); 
-do {
-		printf("Input input file name: \n");
-		scanf(" %s", &input);
-		printf("Input output file name: \n");
-		scanf(" %s", &output);    
- do
-    {
-        printf("Do you want to use algebra or vector calculator? (a/v):\n "); // выборка для пользователя между числами и векторами
-        fscanf(" %c", &x);                                                    
-        switch (x)
-        {
-        case 'a': 
-
-				printf("Input first number:\n"); 
-				fscanf(in, "%f", &a);
-				printf("Input operation (-+/*^!):\n"); 
-				fscanf(in, " %c", &z);
-
-				if (z == '!')
-						{
-					if (a < 0) 
-							{
-						fprintf(out, "Error \n");
-					} else if (a == 0) {
-						fprintf(out, "1! = 1 \n"); 
-					} else if (a - (int) a != 0) 
-							{
-						fprintf(out,
-								"You can't use factorial of this number \n");
-					} else if (a != 0) //
-							{
-						res1 = 1;
-						for (int r = 1; r <= a; r++) {
-							res1 *= r;
-						}
-						fprintf(out, "%f ! = ", a);
-						fprintf(out, "%i \n", res1);
-					}
-				} else {
-					printf("Input second number: \n"); 
-					fscanf(in, "%f", &b);
-					switch (z) 					
-					{
-					case '+':
-						fprintf(out, "%f + ", a);
-						fprintf(out, "%f = ", b);
-						fprintf(out, " %f \n", a + b);
-						break;
-					case '-':
-						fprintf(out, "%f - ", a);
-						fprintf(out, "%f = ", b);
-						fprintf(out, " %f \n", a - b);
-						break;
-					case '/':
-						fprintf(out, "%f / ", a);
-						fprintf(out, "%f = ", b);
-						fprintf(out, " %f \n", a / b);
-						break;
-					case '*':
-						fprintf(out, "%f * ", a);
-						fprintf(out, "%f = ", b);
-						fprintf(out, " %f \n", a * b);
-						break;
-					case '^':
-						g = 1;
-						if (b < 0) {
-							for (int i = 0; i > b; i--) {
-								g = g * a;
-							}
-							res = 1 / g;
-						} else if (b == 0) {
-							res = 1;
-						} else if (b - (int) b != 0) {
-							fprintf(out, "Error \n");
-						} else {
-							for (int i = 1; i <= b; i++) {
-								g = g * a;
-							}
-							res = g;
-						}
-						fprintf(out, "%f ^ ", a);
-						fprintf(out, "%f = ", b);
-						fprintf(out, " %f \n", res);
-						break;
-					default:
-						fprintf(out, "Error \n");
-					}
-				}
-				break;
-
-			case 'v': 
-			{
-				printf("Input size of vectors: \n");
-				fscanf(in, "%i", &i);
-				c = malloc(i * sizeof(float)); 
-				d = malloc(i * sizeof(float)); 
-				printf("Input first vector: \n ");	
-
-				for (j = 0; j < i; j++) {
-					fscanf(in, "%f", &c[j]);
-				}
-				printf("Input operation (-+*): \n");
-				fscanf(in, " %c", &t);
-				printf("Input second vector: \n ");
-				for (j = 0; j < i; j++)		
-						{
-					fscanf(in, "%f", &d[j]);
-				}
-				switch (t) {
-				case '-': 								
-					fprintf(out, "%c ", '(');
-					for (j = 0; j < i; j++)
-					{
-						fprintf(out, "%f ", c[j]);
-					}
-					fprintf(out, "%c", ')');
-					fprintf(out, " %c ", '-');
-					fprintf(out, "%c ", '(');
-					for (j = 0; j < i; j++)
-					{
-						fprintf(out, "%f ",  d[j]);
-					}
-					fprintf(out, "%c", ')');
-					fprintf(out, " %c ", '=');
-					fprintf(out, "%c ", '(');
-					for (j = 0; j < i; j++)
-							{
-						fprintf(out, "%f ", c[j] - d[j]);
-					}
-					fprintf(out, "%c", ')');
-					break;
-				case '+':
-					fprintf(out, "%c", '(');
-					for (j = 0; j < i; j++)
-					{
-						fprintf(out, "%f ", c[j]);
-					}
-					fprintf(out, "%c", ')');
-					fprintf(out, "%c", '+');
-					fprintf(out, "%c", '(');
-					for (j = 0; j < i; j++)
-					{
-						fprintf(out, "%f ",  d[j]);
-					}
-					fprintf(out, "%c", '=');
-					fprintf(out, "%c", '(');
-					for (j = 0; j < i; j++) {
-						fprintf(out, "%f ", c[j] + d[j]);
-					}
-					fprintf(out, "%c", ')');
-					break;
-				case '*':				//цикл вывода скалярного произведения
-					fprintf(out, "%c ", '(');
-					for (j = 0; j < i; j++)
-					{
-						fprintf(out, "%f ", c[j]);
-					}
-					fprintf(out, "%c", ')');
-					fprintf(out, " %c ", '*');
-					fprintf(out, "%c ", '(');
-					for (j = 0; j < i; j++)
-					{
-						fprintf(out, "%f ",  d[j]);
-					}
-					fprintf(out, "%c ", ')');
-					fprintf(out, "%c ", '=');
-					fprintf(out, "%c ", '(');
-					for (j = 0; j < i; j++) {
-						fprintf(out, "%f ", c[j] * d[j]);
-					}
-					fprintf(out, "%c", ')');
-					break;
-				default:
-					fprintf(out, "WRONG OPERATION");	//стоило бы сделать так, чтобы это выводилось сразу после вывода операции, которой нет в перечисленном списке
-				}
-				free(c);
-				free(d);
-				fclose(in);
-				fclose(out);
-			}
-			}
-			printf("Do you want to continue? (y/n) \n");
-			fscanf(in, " %c", &y);
-		} while (y == 'y');
-		printf("Do you want to continue? (y/n) \n");
-		scanf(" %c", &yy);
-	} while (yy == 'y');
-	return 0;
+                            }
+                            fprintf(outFile,"\n");
+                            break;
+                        default:                                 // если операция не определена то будет ошибка
+                            fprintf(outFile,"Error\n");
+                    }
+                } else {
+                    fscanf(inFile," %f",&a);
+                    res = 1;
+                    if (a >= 0 && (a <= (int)a)) {
+                        for (int i = 1; i <= a; i++) {       // факториал реализован через цикл for
+                            res *= i;
+                        }
+                        fprintf(outFile,"%f %c = %f\n", a, operation, res);
+                        fprintf(outFile,"\n");
+                    } else {                                 // обработка ошибки
+                        fprintf(outFile,"Error\n");
+                    }
+                }
+            }
+        } while (feof(inFile) == 0);                             // цикл для того чтобы пользователь продолжил считать либо закончил работу
+        fclose(outFile);        // закрывает файл при завершении работы с файлом ввода
+        clearerr(outFile);     // очистка
+        printf("\nWant to work with new files(input and output)?\n");  // хотите работать с новыми файлами
+        scanf(" %c", &next);
+    }while (next == 'y');
+    return 0;
 }
